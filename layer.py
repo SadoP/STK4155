61cc204fbbfbb34d0c44c394f989ce0cf7d78096
Pascal <pascal.sado@fys.uio.no>
2020-10-16 17:19:34 +0200

    some refactoring

--- layer.py
+++ layer.py
@@ -1,86 +1,37 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+from typing import List
 
 
-class LayerDense:
-    def __init__(self, n_inputs, n_outputs, name, learning_rate=0.1):
-        self.biases = 0.01*np.ones((n_outputs, 1))
-        self.weights = np.random.rand(n_outputs, n_inputs)
-        self.output = np.zeros((n_outputs, 1))
-        self.z = np.array([])
-        self.x = np.array([])
-        self.error = np.array([])
-        self.learning_rate = 0.01
-        self.name = name
-
-    def forward(self, x):
-        self.x = x
-        self.z = np.dot(self.weights, self.x) + self.biases
-        self.output = self.activation(self.z)
+class Costfunctions:
+    @staticmethod
+    def mse(y, yt):
+        return sum((y - yt)**2)
 
     @staticmethod
-    def activation(x):
-        return ActivationFunctions.sigmoid(x)
+    def mse_grad(y, yt):
+        return y - yt
 
     @staticmethod
-    def activation_grad(x) -> np.array:
-        return ActivationFunctions.sigmoid_grad(x)
-
-    def set_error(self, error) -> np.array:
-        self.error = error
-
-    def backward(self):
-        pass
-
-    def weight_grad(self):
-        return np.matmul(self.error, self.x.T)
-
-    def bias_grad(self) -> np.array:
-        return np.sum(self.error, axis=0)
-
-    def back_propagation(self):
-        next_error = np.matmul(self.weights.T, self.error) * self.activation_grad(self.x)
-        self.biases = self.biases + self.learning_rate * self.bias_grad()
-        self.weights = self.weights + self.learning_rate * self.weight_grad()
-        return next_error
-
-
-class SoftMax:
-    def __init__(self, n_inputs, n_outputs):
-        self.biases = 0.01*np.ones((n_outputs, 1))
-        self.weights = np.random.rand(n_outputs, n_inputs)
-        self.output = np.zeros((n_outputs, 1))
-        self.z = np.array([])
-        self.x = np.array([])
-        self.error = np.array([])
-        self.learning_rate = 0.01
-
-    def forward(self, x):
-        self.x = x
-        self.z = np.dot(self.weights, self.x) + self.biases
-        self.output = self.activation(self.z)
+    def cross_entropy(y, yhat):
+        """
+        Calculates cross entropy loss
+        :param y: true result
+        :param yhat: calculated / predicted result
+        :return:
+        """
+        return -np.sum(y*np.log(yhat) + (1-y)*np.log(1-yhat))
 
     @staticmethod
-    def activation(x):
-        return ActivationFunctions.probability(x)
-
-    @staticmethod
-    def activation_grad(x) -> np.array:
-        return ActivationFunctions.probability_grad(x)
-
-    def set_error(self, error) -> np.array:
-        self.error = error
-
-    def weight_grad(self):
-        return np.matmul(self.error, self.x.T)
-
-    def bias_grad(self) -> np.array:
-        return np.sum(self.error, axis=0)
-
-    def back_propagation(self):
-        next_error = np.matmul(self.weights.T, self.error) * self.activation_grad(self.x)
-        self.biases = self.biases + self.learning_rate * self.bias_grad()
-        self.weights = self.weights + self.learning_rate * self.weight_grad()
-        return next_error
+    def cross_entropy_grad(y, yhat):
+        """
+        Calculates derivative of cross entropy loss
+        :param y: true result
+        :param yhat: calculated / predicted result
+        :return:
+        """
+        return (yhat - y)/(yhat - yhat**2)
 
 
 class ActivationFunctions:
@@ -93,49 +44,164 @@ class ActivationFunctions:
         return ActivationFunctions.sigmoid(x)*(1-ActivationFunctions.sigmoid(x))
 
     @staticmethod
-    def probability(x):
+    def softmax(x):
         z = np.exp(x)
         return z / np.sum(z, axis=0, keepdims=True)
 
     @staticmethod
-    def probability_grad(x):
+    def softmax_grad(x):
         z = np.exp(x)
         a = np.sum(z)
         return x * (a-x)/a
 
+    @staticmethod
+    def relu(x):
+        return (x > 0)*x
+
+    @staticmethod
+    def relu_grad(x):
+        return (x > 0)*1
+
+
+class Layer:
+    def __init__(self, n_outputs, name, learning_rate, cf, af):
+        self.output = np.zeros((n_outputs, 1))
+        self.z = np.array([])
+        self.x = np.array([])
+        self.error = np.array([])
+        self.initial_learning_rate = learning_rate
+        self.learning_rate = learning_rate
+        self.name = name
+        self.epoch = 0
+        self.cf = cf
+        self.cf_grad = getattr(Costfunctions, cf.__name__+"_grad")
+        self.af = af
+        self.af_grad = getattr(ActivationFunctions, af.__name__+"_grad")
+
+    def forward(self, x):
+        pass
+
+    def backwards(self, error):
+        pass
+
+    def activation(self, x):
+        return self.af(x)
+
+    def activation_grad(self, x):
+        return self.af_grad(x)
+
+    def cost_function(self, y, yt):
+        return self.cf(y, yt)
+
+    def cost_grad(self, y, yt):
+        return self.cf_grad(y, yt)
+
+    def increment_epoch(self):
+        self.epoch += 1
+
+
+class LayerDense(Layer):
+    def __init__(self,n_inputs, n_outputs, name, learning_rate, cost, activation):
+        Layer.__init__(self, n_outputs, name, learning_rate, cost, activation)
+        self.biases = 0.01*np.ones((n_outputs, 1))
+        self.weights = np.random.rand(n_outputs, n_inputs)
+        self.initial_weights = self.weights
+        self.initial_biases = self.biases
+
+    def forward(self, x):
+        self.x = x
+        self.z = np.dot(self.weights, self.x) + self.biases
+        self.output = self.activation(self.z)
+
+    def weight_grad(self):
+        return np.matmul(self.error, self.x.T)
+
+    def bias_grad(self):
+        return np.expand_dims(np.sum(self.error, axis=1), axis=1)
+
+    def backwards(self, error):
+        self.error = error
+        next_error = np.matmul(self.weights.T, self.error) * self.activation_grad(self.x)
+        self.biases = self.biases + self.learning_rate * self.bias_grad()
+        self.weights = self.weights + self.learning_rate * self.weight_grad()
+        return next_error
+
+
+class Network:
+    def __init__(self, layers: List[Layer]):
+        self.layers = layers
+        self.C = []
+
+    def train(self, x, y, epochs, batch_size):
+        self.C = []
+        batches = int(np.floor(x.shape[1] / batch_size))
+        for i in range(epochs):
+            for j in range(batches):
+                xn = x[:, j * batch_size:(j + 1) * batch_size]
+                yn = y[:, j * batch_size:(j + 1) * batch_size]
+                self.forward_pass(xn)
+                error = layers[-1].cost_function(yn, self.layers[-1].output)
+                self.backward_pass(self.layers[-1].cost_grad(yn, self.layers[-1].output))
+            self.C.append(np.sum(error ** 2))
+            network.inc_epoch()
+
+    def predict(self, x):
+        return self.forward_pass(x)
+
+    def forward_pass(self, x):
+        for layer in self.layers:
+            layer.forward(x)
+            x = layer.output
+        return x
+
+    def backward_pass(self, error):
+        for layer in reversed(self.layers):
+            error = layer.backwards(error)
+
+    def inc_epoch(self):
+        for layer in self.layers:
+            layer.increment_epoch()
+
+    def adapt_learning_rate(self, lr):
+        print("new learning rate" + str(lr))
+        for layer in self.layers:
+            layer.learning_rate = lr
+
+
+def test_fun(x):
+    return np.expand_dims(np.sum(x, axis=1), 1)
 
 RANDOM_SEED = 1337
 np.random.seed(RANDOM_SEED)
 n_inputs = 5
 n_middle1 = 64
-n_middle2 = 128
-n_outputs = 5
-n_samples = 5
-#input = np.array([[-5, 2, 3, 7, 0], [6, 10, -2, -5, -3]])
-#y = np.array([[1,2,3,4,5]]).T
-input = np.random.rand(n_samples, n_inputs).T
-y = np.random.rand(n_samples, n_inputs)
-y = y / np.sum(y, axis=1).reshape(n_samples, 1)
+n_middle2 = 1
+n_outputs = 1
+n_samples = 1000
+batch_size = 100
+split_size = 0.8
+epochs = 250
 
-ld1 = LayerDense(n_inputs, n_middle1, "ld1")
-ld2 = LayerDense(n_middle1, n_middle2, "ld2")
-ls = SoftMax(n_middle2, n_outputs)
-err = []
-out = []
-wei = []
-bia = []
-C = []
 
-for i in range(200):
-    ld1.forward(input)
-    ld2.forward(ld1.output)
-    ls.forward(ld2.output)
-    error = y.T - ls.output
-    ls.set_error(error)
-    ld2.set_error(ls.back_propagation())
-    ld1.set_error(ld2.back_propagation())
-    ld1.back_propagation()
-    err.append(ls.error)
-    out.append(ls.output)
-    C.append(np.sum(error**2))
+
+
+input = np.random.rand(n_samples, n_inputs)
+y = test_fun(input)
+#input = scale(input, axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(input, y, train_size=split_size)
+
+
+ld1 = LayerDense(n_inputs, n_middle1, "ld1", 0.00001, Costfunctions.mse, ActivationFunctions.sigmoid)
+ld2 = LayerDense(n_middle1, n_middle2, "ld2", 0.00001, Costfunctions.mse, ActivationFunctions.relu)
+#ls = LayerDenseSoftMax(n_middle2, n_outputs, "ls1", 0.001)
+layers = [ld1, ld2]
+network = Network(layers)
+
+network.train(x_train.T, y_train.T, epochs, batch_size)
+print(network.C)
+
+
+print(np.sum((network.predict(x_test.T) - y_test.T)**2))
+#print(network.layers[-1].error)
 
